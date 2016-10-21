f9691c890c87152b0cf3d9fdacd7bfa18c20bfc8
Millie Walsh <millie@primeacademy.io>
2016-10-21 13:20:48 -0500

    tau is awesome and init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### On-site Tau 10-21-16
+
+[X] html file
+[X] source a js file
+[X] clickable button that will run a function that console logs a message
+[X]  display message on the DOM 
+[] git commit push to github
---
+++ scripty.js
@@ -0,0 +1,8 @@
+console.log('test');
+
+var sayHello = function(){
+  console.log('Hello Tau!');
+
+  //display message hello on the DOM
+  document.getElementById('outputText').innerHTML = 'Hello World!';
+};// end sayHello
